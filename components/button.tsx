0748b023529a828584af8cbdc94323b5b1a23e03
Rahul Rajan <rahul.rajanus@gmail.com>
2021-04-05 16:31:23 -0400

    initial image container

--- components/button.tsx
+++ components/button.tsx
@@ -9,10 +9,10 @@ export default function Button({ text, type }) {
   if (type == ButtonType.Outlined) {
     return (
       <button className={`${styles.button} ${styles.outlined}`}>{text}</button>
-    )
+    );
   } else {
     return (
       <button className={`${styles.button} ${styles.solid}`}>{text}</button>
-    )
+    );
   }
 }
--- pages/timeline.tsx
+++ pages/timeline.tsx
@@ -1,20 +1,119 @@
 import { GetServerSideProps } from "next";
+import styles from "./timeline.module.css";
+import Image from "next/image";
+import Button, { ButtonType } from "../components/button";
 
 interface Props {}
 
-export default function Timeline(props: Props) {
+interface Image {
+  alt: string;
+  path: string;
+}
+
+interface Event {
+  title: string;
+  dates: string;
+  description: string;
+  images: Image[];
+}
+
+const events: Event[] = [
+  {
+    title: "HackGT 7: Reimagine Reality",
+    dates: "October 16-18, 2020",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+    images: [
+      {
+        alt: "girl working on project at Horizons",
+        path: "/hackgt7-1.jpg"
+      },
+      {
+        alt: "girl working on project at Horizons",
+        path: "/hackgt7-1.jpg"
+      }
+    ]
+  },
+  {
+    title: "HackGT 7: Reimagine Reality",
+    dates: "October 16-18, 2020",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+    images: [
+      {
+        alt: "girl working on project at Horizons",
+        path: "/hackgt7-1.jpg"
+      }
+    ]
+  },
+  {
+    title: "HackGT 7: Reimagine Reality",
+    dates: "October 16-18, 2020",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+    images: [
+      {
+        alt: "girl working on project at Horizons",
+        path: "/hackgt7-1.jpg"
+      }
+    ]
+  }
+];
+
+function ImageContainer(images: Image[]) {
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className={styles.imageContainer}>
+      {images.map(image => {
+        return (
+          <div
+            style={{
+              height: "375px",
+              width: "100%"
+            }}
+          >
+            <div
+              style={{
+                position: "relative",
+                maxWidth: "100%",
+                height: "100%"
+              }}
+            >
+              <Image
+                src={image.path}
+                alt={image.alt}
+                layout="fill"
+                objectFit="contain"
+                quality={100}
+              />
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 }
 
-export const getServerSideProps: GetServerSideProps<Props> = async (
-  context
-) => {
+function Event(event: Event) {
+  return (
+    <div className={styles.event}>
+      <h1>{event.title}</h1>
+      <h2>{event.dates}</h2>
+      <p>{event.description}</p>
+      {ImageContainer(event.images)}
+      <div>
+        <Button text="Website" type={ButtonType.Outlined} />
+      </div>
+    </div>
+  );
+}
+
+export default function Timeline(props: Props) {
+  return <div>{events.map(Event)}</div>;
+}
+
+export const getServerSideProps: GetServerSideProps<Props> = async context => {
   // Make request here for data
   return {
-    props: {}, // will be passed to the page component as props
+    props: {} // will be passed to the page component as props
   };
 };
