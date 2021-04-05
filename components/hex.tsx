a83af50ee91e9c2393992a19a31b3e5b4a071004
Elton <eltonp3103@gmail.com>
2021-04-05 16:43:27 -0400

    switch hex to svg

--- components/hex.tsx
+++ components/hex.tsx
@@ -19,58 +19,49 @@
   *         borderColor="black"
   *     />
   */
-export default function Hex({ size, rotation, image, color, borderSize, borderColor }) {
+export default function Hex({ x, y, size, rotation, image, color, borderSize, borderColor }) {
   // height = 0.5*sqrt(3)*width
   const height = (width) => 0.8660254 * width;
-  const [borderWidth, borderHeight] = [size + borderSize, height(size + borderSize)];
+  const [borderWidth, borderHeight] = [size + 2 * borderSize, height(size + 2 * borderSize)];
   const shape = "polygon(25% 0%, 75% 0%, 100% 50%, 75% 100%, 25% 100%, 0% 50%)";
-
+  
+  const dx = size / 2;
+  const dy = Math.sqrt(3) * dx;
+  
   return (
     <div
       style={{
-        position: "relative",
+        position: "absolute",
+        top: y,
+        left: x,
         width: `${borderWidth}px`,
         height: `${borderHeight}px`,
         transform: `rotate(${rotation}deg)`,
       }}
     >
-      <div
-        style={{
-          position: "absolute",
-          top: "50%",
-          left: "50%",
-          width: `${size}px`,
-          height: `${height(size)}px`,
-          transform: "translate(-50%, -50%)",
-          clipPath: shape,
-          backgroundColor: color,
-          backgroundImage: `url(${image})`,
-          backgroundRepeat: "no-repeat",
-          backgroundSize: "cover",
-          zIndex: 100,
-        }}
-      />
-      <div
-        style={{
-          position: "absolute",
-          top: "0px",
-          left: "0px",
-          width: `${borderWidth}px`,
-          height: `${borderHeight}px`,
-          clipPath: shape, 
-          backgroundColor: borderColor,
-          zIndex: 90
-        }}
-      />
+      <svg width={size + 2 * (dx + borderSize)} height={2 * (dy + borderSize)}>
+        <path d={`
+          m ${dx} ${borderSize} 
+          l ${size} 0
+          l ${dx} ${dy}
+          l ${-dx} ${dy}
+          l ${-size} 0
+          l ${-dx} ${-dy}
+          z`}
+          stroke={borderColor}
+          stroke-width={borderSize}
+          fill={color}
+        />
+      </svg>
     </div>
-  );
+  )
 }
 
 Hex.defaultProps = {
-  size: 400,
+  size: 200,
   rotation: 0,
   image: "none",
   color: "var(--purple)",
-  borderColor: "black",
+  borderColor: "transparent",
   borderSize: 0,
 };
