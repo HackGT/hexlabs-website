f1894921638aeb1ea1d275812cabde1c228aaa0d
Elton <eltonp3103@gmail.com>
2021-04-08 11:25:26 -0400

    add rebranding banner

--- components/header.tsx
+++ components/header.tsx
@@ -1,8 +1,16 @@
 import NavBar from "./navbar";
+import Banner from "./banner";
+import Link from "next/link";
 
 export default function Header() {
   return (
     <header>
+      <Banner>
+        We have 
+        <Link href="/rebranded">
+          <a> rebranded! </a>
+        </Link>
+      </Banner>
       <NavBar/> 
     </header>
   )
