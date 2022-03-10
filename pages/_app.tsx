5d6492e472cf3a4171c73f370ad289f0047ae45b
Ayush Goyal <ayush-goyal@users.noreply.github.com>
2022-03-10 02:05:51 -0500

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -87,6 +87,7 @@ export default function App({ Component, pageProps }: AppProps) {
             name="viewport"
             content="width=device-width, initial-scale=1.0"
           />
+          <meta name="google-site-verification" content="gwllFByGUzdKQVLum5RmdEAK5u_rdOgmD61KmwLLISI" />
         </Head>
         <Header />
         <main>
