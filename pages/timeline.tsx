2b64b738be7b6fe816972c13596d1174df9b9bf7
Yu Pan <70486009+yu-pan-cpu@users.noreply.github.com>
2022-04-05 00:58:25 -0400

    fix unique key warning; remove Image usage

--- pages/timeline.tsx
+++ pages/timeline.tsx
@@ -2,12 +2,13 @@ import Hex from "../components/hex";
 import { GetServerSideProps } from "next";
 import styles from "./timeline.module.css";
 import { Text } from "@chakra-ui/react";
-import Image from "next/image";
 import { Button, ButtonType } from "../components/button";
+import { imageConfigDefault } from "next/dist/server/image-config";
 
 interface Props {}
 
-interface Image {
+interface Img {
+  id: string;
   alt: string;
   path: string;
 }
@@ -17,7 +18,7 @@ interface Event {
   dates: string;
   description: string;
   devPostLink: string;
-  images: Image[];
+  images: Img[];
 }
 
 const events: Event[] = [
@@ -26,21 +27,24 @@ const events: Event[] = [
     dates: "October 16 - 18, 2020",
     description:
       "HackGTs 7th annual, and first entirely virtual, installment of our marquee collegiate hackathon! In 2020, 2500 participants virtually came together to embark on a 36-hour adventure to ‘Reimagine Reality’. Our goal was to redefine what it means to have an incredible hacker experience through virtual workshops, mentors, and tech talks. We hoped to spark our participants’ imagination and creativity as they reimagined and created their own world! ",
-    devPostLink: "https://hackgt2020.devpost.com/",  
+    devPostLink: "https://hackgt2020.devpost.com/",
     images: [
       {
+        id: "7-1",
         alt: "girl working on project at Horizons",
-        path: "/hackgt7-1.png"
+        path: "/hackgt7-1.png",
       },
       {
+        id: "7-2",
         alt: "girl working on project at Horizons",
-        path: "/hackgt7-2.png"
+        path: "/hackgt7-2.png",
       },
       {
+        id: "7-3",
         alt: "girl working on project at Horizons",
-        path: "/hackgt7-3.png"
-      }
-    ]
+        path: "/hackgt7-3.png",
+      },
+    ],
   },
   {
     title: "HackGT 6: Into the Rabbit Hole",
@@ -50,18 +54,21 @@ const events: Event[] = [
     devPostLink: "https://hackgt2019.devpost.com/",
     images: [
       {
+        id: "6-1",
         alt: "girl working on project at Horizons",
-        path: "/hackgt6-1.png"
+        path: "/hackgt6-1.png",
       },
       {
+        id: "6-2",
         alt: "girl working on project at Horizons",
-        path: "/hackgt6-2.png"
+        path: "/hackgt6-2.png",
       },
       {
+        id: "6-3",
         alt: "girl working on project at Horizons",
-        path: "/hackgt6-3.png"
-      }
-    ]
+        path: "/hackgt6-3.png",
+      },
+    ],
   },
   {
     title: "HackGT 5: Dare to Venture",
@@ -71,18 +78,21 @@ const events: Event[] = [
     devPostLink: "https://hackgt2018.devpost.com/",
     images: [
       {
+        id: "5-1",
         alt: "girl working on project at Horizons",
-        path: "/hackgt5-1.png"
+        path: "/hackgt5-1.png",
       },
       {
+        id: "5-2",
         alt: "girl working on project at Horizons",
-        path: "/hackgt5-2.png"
+        path: "/hackgt5-2.png",
       },
       {
+        id: "5-3",
         alt: "girl working on project at Horizons",
-        path: "/hackgt5-3.png"
+        path: "/hackgt5-3.png",
       },
-    ]
+    ],
   },
   {
     title: "HackGT 4: New Horizons",
@@ -92,18 +102,21 @@ const events: Event[] = [
     devPostLink: "https://hackgt2017.devpost.com/",
     images: [
       {
+        id: "4-1",
         alt: "girl working on project at Horizons",
-        path: "/hackgt4-1.png"
+        path: "/hackgt4-1.png",
       },
       {
+        id: "4-2",
         alt: "girl working on project at Horizons",
-        path: "/hackgt4-2.png"
+        path: "/hackgt4-2.png",
       },
       {
+        id: "4-3",
         alt: "girl working on project at Horizons",
-        path: "/hackgt4-3.png"
+        path: "/hackgt4-3.png",
       },
-    ]
+    ],
   },
   {
     title: "HackGT 3: Flow",
@@ -113,18 +126,21 @@ const events: Event[] = [
     devPostLink: "https://hackgt2016.devpost.com/",
     images: [
       {
+        id: "3-1",
         alt: "girl working on project at Horizons",
-        path: "/hackgt3-1.png"
+        path: "/hackgt3-1.png",
       },
       {
+        id: "3-2",
         alt: "girl working on project at Horizons",
-        path: "/hackgt3-2.png"
+        path: "/hackgt3-2.png",
       },
       {
+        id: "3-3",
         alt: "girl working on project at Horizons",
-        path: "/hackgt3-3.png"
+        path: "/hackgt3-3.png",
       },
-    ]
+    ],
   },
   {
     title: "HackGT 2",
@@ -134,18 +150,21 @@ const events: Event[] = [
     devPostLink: "https://fall2015.hack.gt/",
     images: [
       {
+        id: "2-1",
         alt: "girl working on project at Horizons",
-        path: "/hackgt2-1.png"
+        path: "/hackgt2-1.png",
       },
       {
+        id: "2-2",
         alt: "girl working on project at Horizons",
-        path: "/hackgt2-2.png"
+        path: "/hackgt2-2.png",
       },
       {
+        id: "2-3",
         alt: "girl working on project at Horizons",
-        path: "/hackgt2-3.png"
+        path: "/hackgt2-3.png",
       },
-    ]
+    ],
   },
   {
     title: "HackGT 1",
@@ -155,58 +174,59 @@ const events: Event[] = [
     devPostLink: "https://hackgt2014.devpost.com/",
     images: [
       {
+        id: "1-1",
         alt: "girl working on project at Horizons",
-        path: "/hackgt1-1.png"
+        path: "/hackgt1-1.png",
       },
       {
+        id: "1-2",
         alt: "girl working on project at Horizons",
-        path: "/hackgt1-2.png"
+        path: "/hackgt1-2.png",
       },
       {
+        id: "1-3",
         alt: "girl working on project at Horizons",
-        path: "/hackgt1-3.png"
+        path: "/hackgt1-3.png",
       },
-    ]
-  }
+    ],
+  },
 ];
 
-function ImageContainer(images: Image[]) {
+function ImageContainer(props) {
   return (
     <div className={styles.imageContainer}>
-      {images.map(image => {
-        return (
-          <img src={image.path} alt={image.alt} className={styles.image} />
-        );
-      })}
+      <img src={props.image.path} alt={props.image.alt} className={styles.image} />
     </div>
   );
 }
 
 function Event(event: Event) {
+  const images = event.images.map((img) => <ImageContainer key={img.id} image={img} />);
   return (
     <>
       <div className={styles.event}>
-          <Hex
-              size={25}
-              borderSize={"8"}
-              borderColor={"var(--color-blue)"}
-              color={"white"}
-              x={"-26px"}
-              y={"0px"}
-            />
-            <div className={styles.topContainer}>
-              <div className={styles.eventDescription}>
-                <Text as="h3">{event.title}</Text>
-                <h4>{event.dates}</h4>
-                <p>{event.description}</p>
-              </div>
-            </div>
-
-            {ImageContainer(event.images)}
-            <div className={styles.buttonContainer}>
-              <a target="_blank" href={event.devPostLink}><Button type={ButtonType.OUTLINED}>Devpost</Button></a>
-            </div>
+        <Hex
+          size={25}
+          borderSize={"8"}
+          borderColor={"var(--color-blue)"}
+          color={"white"}
+          x={"-26px"}
+          y={"0px"}
+        />
+        <div className={styles.topContainer}>
+          <div className={styles.eventDescription}>
+            <Text as="h3">{event.title}</Text>
+            <h4>{event.dates}</h4>
+            <p>{event.description}</p>
           </div>
+        </div>
+        {images}
+        <div className={styles.buttonContainer}>
+          <a target="_blank" href={event.devPostLink}>
+            <Button type={ButtonType.OUTLINED}>Devpost</Button>
+          </a>
+        </div>
+      </div>
     </>
   );
 }
@@ -217,31 +237,24 @@ export default function Timeline(props: Props) {
       <section id="styles.customMargin" className="splash">
         <h1>HackGT</h1>
         <p className="max-width-600">
-          Hackathons provide the community with the opportunity to grow, create, and impact. At HackGT, HexLabs teams up with sponsors 
-          from our community to provide issues relevant to today’s world for our HackGT participants to solve, provide workshops to help 
-          students learn, and prizes for winners!
+          Hackathons provide the community with the opportunity to grow, create, and impact. At
+          HackGT, HexLabs teams up with sponsors from our community to provide issues relevant to
+          today’s world for our HackGT participants to solve, provide workshops to help students
+          learn, and prizes for winners!
         </p>
         <div className="hex-container">
-          <Hex
-            size={300}
-            rotation={45}
-            image="/timeline-splash.png"
-            x="60%"
-            y="9%"
-          />
+          <Hex size={300} rotation={45} image="/timeline-splash.png" x="60%" y="9%" />
         </div>
       </section>
 
-      <div className={styles.timelineStyle}>
-        {events.map(Event)}
-      </div>
+      <div className={styles.timelineStyle}>{events.map(Event)}</div>
     </div>
   );
 }
 
-export const getServerSideProps: GetServerSideProps<Props> = async context => {
+export const getServerSideProps: GetServerSideProps<Props> = async (context) => {
   // Make request here for data
   return {
-    props: {} // will be passed to the page component as props
+    props: {}, // will be passed to the page component as props
   };
 };
